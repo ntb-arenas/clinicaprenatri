9f65ab796f60cb4b762f768204c625a62697d63e
Niño Arenas <ntb.arenas@protonmail.com>
2022-10-18 00:22:36 +0100

    fix burger scroll

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const body = document.querySelector("body");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+  body.classList.toggle("touch-none");
 });
