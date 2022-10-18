1a3022708f35e9584bfda462fb9f62f62faaa97e
Niño Arenas <ntb.arenas@protonmail.com>
2022-10-18 12:05:01 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-const header = document.querySelector("header");
+// const header = document.querySelector("header");
+const body = document.querySelector("body");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
-  header.classList.toggle("touch-none");
+  // header.classList.toggle("touch-none");
+  body.classList.toggle("touch-none");
 });
